3c5f99edbfd57b2d5174b59c59e53356447354a2
jamiyanmyadag <jamiynmyadag@gmail.com>
2018-09-27 02:31:44 +0800

    deploy

--- index.js
+++ index.js
@@ -124,7 +124,19 @@ app.get('/webhook', (req, res) => {
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
-
+    let response;
+  
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+    
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+      response = { "text": "Thanks!" }
+    } else if (payload === 'no') {
+      response = { "text": "Oops, try sending another image." }
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
 }
 
 // Sends response messages via the Send API
