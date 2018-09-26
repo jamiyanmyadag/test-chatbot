e46c47843328986348918e2e1f5ec66d2a8b9d09
jamiyanmyadag <jamiynmyadag@gmail.com>
2018-09-27 02:29:53 +0800

    deploy

--- index.js
+++ index.js
@@ -78,25 +78,8 @@ app.get('/webhook', (req, res) => {
 
 
   // Handles messages events
-function handleMessage(sender_psid, received_message) {
+  function handleMessage(sender_psid, received_message) {
     let response;
-
-    // Check if the message contains text
-    if (received_message.text) {    
-
-      // Create the payload for a basic text message
-      response = {
-        "text": `You sent the message: "${received_message.text}". Now send me an image!`
-      }
-    }  
-
-    // Sends the response message
-    callSendAPI(sender_psid, response); 
-}
-
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-      let response;
     
     // Checks if the message contains text
     if (received_message.text) {    
@@ -136,7 +119,12 @@ function handlePostback(sender_psid, received_postback) {
     } 
     
     // Send the response message
-    callSendAPI(sender_psid, response);  
+    callSendAPI(sender_psid, response);    
+  }
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
 }
 
 // Sends response messages via the Send API
